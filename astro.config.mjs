b048bc085beb7f38a7e7dba5848f021324290540
Dianlestari09 <230605110016@student.uin-malang.ac.id>
2026-07-25 23:55:02 +0700

    fix: bundle config for tesseract on vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,15 @@ import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
+  adapter: vercel({
+    includeFiles: [
+      './node_modules/tesseract.js/**/*',
+      './node_modules/tesseract.js-core/**/*'
+    ]
+  }),
+  vite: {
+    ssr: {
+      external: ['tesseract.js']
+    }
+  }
 });
